6de0f8c7d680722efa123013f7fecebf4bb95fb8
jeff <jeff.etale@student.moringaschool.com>
2023-09-06 11:44:21 +0300

    done lab

---
+++ lib/migrations/versions/71d32a7966b3_created_game_model.py
@@ -0,0 +1,24 @@
+"""Created Game Model
+
+Revision ID: 71d32a7966b3
+Revises: faebd12f9523
+Create Date: 2023-09-06 11:21:25.338822
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '71d32a7966b3'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
